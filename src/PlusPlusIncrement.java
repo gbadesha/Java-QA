d4b47c066f21e0e50b81a2ea55942e1337256909
gbadesha <gurpreetmba@gmail.com>
2023-02-09 20:49:28 -0500

    new file

---
+++ src/PlusPlusIncrement.java
@@ -0,0 +1,22 @@
+public class PlusPlusIncrement {
+    public static void main(String[] args)
+    {
+        //int z=0;
+        /*do
+        {
+            System.out.println("a= " +a);
+            ++a;
+        }
+        while(a<1);*/
+        for(int i=1; i<=5; i++ ) {
+            for(int z=i;z<=5;z++){
+                System.out.print("*");
+            }
+                        System.out.println();
+            }
+            //System.out.print( i + " ");
+                        //i=i+1;
+        }
+
+
+}
